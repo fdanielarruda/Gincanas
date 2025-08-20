7975aa12bbdc5c3f7975e48fc3a159d8ee199c6c
Daniel Arruda <arruda.daniel20@gmail.com>
2025-08-20 14:13:17 -0300

    refatorar: atualizar cabeçalhos da tabela de fases para melhor clareza e exibição

--- resources/js/Pages/Gymkhana/Phases/Index.vue
+++ resources/js/Pages/Gymkhana/Phases/Index.vue
@@ -71,13 +71,10 @@ const removePhase = performDeletion;
                                     <tr>
                                         <th scope="col"
                                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">
-                                            Título</th>
+                                            Fase</th>
                                         <th scope="col"
                                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">
-                                            Descrição</th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">
-                                            Detalhes</th>
+                                            Pontuação</th>
                                         <th scope="col" class="relative px-6 py-3"><span class="sr-only">Ações</span>
                                         </th>
                                     </tr>
